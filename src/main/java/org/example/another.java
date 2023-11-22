46ea3755efdc75c26a66752729e08c92699cc5bb
jubayerh <jubayer@hotmail.com>
2023-11-22 12:04:43 -0500

    2-line added

--- src/main/java/org/example/another.java
+++ src/main/java/org/example/another.java
@@ -4,5 +4,6 @@ public class another {
 
     public static void main(String[] args) {
         System.out.println("this in add new class");
+        System.out.println("this is second line");
     }
 }
